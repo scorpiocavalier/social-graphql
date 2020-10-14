a136f0f6e9688c31dda96fecfa278a4166732d65
Arturo Chen <art.can.fly@gmail.com>
2020-10-14 04:34:14 -0400

    + user login validation

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -4,7 +4,19 @@ const jwt = require('jsonwebtoken')
 const User = require('../../models/User')
 const { SECRET_KEY } = require('../../config')
 const { UserInputError } = require('apollo-server')
-const { validateRegisterInput } = require('../../utils/validators')
+const { validateRegisterInput, validateLoginInput } = require('../../utils/validators')
+
+const generateToken = user => (
+  jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username
+    },
+    SECRET_KEY,
+    { expiresIn: '1h' }
+  )
+)
 
 module.exports = {
   Mutation: {
@@ -46,17 +58,36 @@ module.exports = {
       const res = await newUser.save()
 
       // create an auth token
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username
-        },
-        SECRET_KEY,
-        { expiresIn: '1h' }
-      )
+      const token = generateToken(res)
 
       return { ...res._doc, id: res._id, token }
+    },
+
+    login: async (parent, args, context, info) => {
+      const { username, password } = args
+      const { errors, valid } = validateLoginInput(username, password)
+
+      if (!valid) {
+        throw new UserInputError('Errors', { errors })
+      }
+
+      const user = await User.findOne({ username })
+
+      if (!user) {
+        errors.general = 'User not found'
+        throw new UserInputError('User not found', { errors })
+      }
+
+      const match = await bcrypt.compare(password, user.password)
+
+      if (!match) {
+        errors.general = 'Wrong credentials'
+        throw new UserInputError('Wrong credentials', { errors })
+      }
+
+      const token = generateToken(user)
+
+      return { ...user._doc, id: user._id, token }
     }
   }
 }
